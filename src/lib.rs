c91b1e837a95a64746cdee2ac6a989a4fee539c7
Oleg Syniakevych <olegsyniakevych@gmail.com>
2023-11-12 16:48:15 +0200

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! This module contains the implementation of a parser for workout data.
+//!
+//! The parser is implemented using the pest parser generator and is used to parse
+//! workout data in a custom format. The parsed data is then used to generate
+//! workout plans and track progress.
+
 use pest::{iterators::Pair, Parser};
 use serde::{Deserialize, Serialize};
 
@@ -9,21 +15,38 @@ use error::Error;
 
 #[derive(Parser)]
 #[grammar = "workout.pest"]
-pub struct WorkoutParser;
+struct WorkoutParser;
 
+/// Represents a set in weightlifting, consisting of a weight and the number of repetitions performed.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Set {
-    weight: f32,
-    n_reps: i32,
+    /// The weight used for this set.
+    pub weight: f32,
+    /// The number of repetitions performed for this set.
+    pub n_reps: i32,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+/// A struct representing an exercise, actually, a single set
 pub struct Exercise {
-    name: String,
-    sets: Vec<Set>,
-    comment: Option<String>,
+    // the name of your exercise
+    pub name: String,
+    // this is a vector because you can do two sets without a rest, and this counts as a single set
+    pub sets: Vec<Set>,
+    // a comment about the exercise
+    pub comment: Option<String>,
 }
 
+/// Parses a set of exercises from a `Pair` object.
+///
+/// # Arguments
+///
+/// * `name` - A `String` representing the name of the exercise.
+/// * `pair` - A `Pair` object representing the set of exercises to parse.
+///
+/// # Returns
+///
+/// Returns a `Result` containing an `Exercise` object if parsing was successful, or an `Error` if parsing failed.
 pub fn parse_set(name: String, pair: Pair<'_, Rule>) -> Result<Exercise, Error> {
     if pair.as_rule() != Rule::set {
         return Err(Error::ExpectedRule(Rule::set));
@@ -68,6 +91,15 @@ pub fn parse_set(name: String, pair: Pair<'_, Rule>) -> Result<Exercise, Error> 
     })
 }
 
+/// Parses an exercise from a `Pair` and returns a vector of `Exercise`s.
+///
+/// # Arguments
+///
+/// * `pair` - A `Pair` that represents an exercise.
+///
+/// # Returns
+///
+/// A `Result` containing a vector of `Exercise`s if parsing was successful, or an `Error` if parsing failed.
 pub fn get_exercise_from_pairs(pair: Pair<'_, Rule>) -> Result<Vec<Exercise>, Error> {
     if pair.as_rule() != Rule::exercise {
         return Err(Error::ExpectedRule(Rule::exercise));
@@ -87,6 +119,15 @@ pub fn get_exercise_from_pairs(pair: Pair<'_, Rule>) -> Result<Vec<Exercise>, Er
         .collect()
 }
 
+/// Parses a workout input string and returns a vector of exercises.
+///
+/// # Arguments
+///
+/// * `input` - A string slice that holds the workout input.
+///
+/// # Returns
+///
+/// * `Result<Vec<Exercise>, Error>` - A `Result` that holds a vector of `Exercise`s if parsing was successful, or an `Error` if parsing failed.
 pub fn parse_workout(input: &str) -> Result<Vec<Exercise>, Error> {
     let mut parsed = WorkoutParser::parse(Rule::workout, input)?;
 
